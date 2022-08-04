4c3be44bc67f0e23ce52fd3985711e24099901c1
Dayvson Silva <userddssilva@gmail.com>
2022-08-04 05:18:21 -0400

    refactor: change project ts to js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import App from './component/App';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
